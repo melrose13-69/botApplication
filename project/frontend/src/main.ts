63d7265f97860e0b5ac449309af6bcdeac7bdd2e
vpoltoratchi <vpoltoratchi@noction.com>
2022-04-29 15:57:38 +0300

    test

--- project/frontend/src/main.ts
+++ project/frontend/src/main.ts
@@ -5,8 +5,5 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-const x=1
-const y = { a:1 }
 
-console.log(x, y)
 createApp(App).use(store).use(router).mount('#app')
